d1aace05f36993618087d6d381a4e2706b1ed6cc
Scottin3d <scottin3d@gmail.com>
2020-08-21 17:04:14 -0700

    updates

--- Assets/firstPersonController.cs
+++ Assets/firstPersonController.cs
@@ -4,21 +4,91 @@ using UnityEngine;
 
 public class firstPersonController : MonoBehaviour
 {
+    private Rigidbody rigidbody;
+
 
     public float mouseSensitivityX = 250f;
     public float mouseSensitivityY = 250f;
 
-    Transform camera;
+    private Transform camera;
+    private float verticalLookRotation;
+
+    private Vector3 moveAmount;
+    [SerializeField]
+    private float moveSpeed = 2; // player speed
+    // boost speed clamp
+    // boost multiplier
+    private Vector3 smoothMoveVelocity;
+
+    [SerializeField]
+    private float jumpForce = 220f;
+    private bool isGrounded = true;
+    [SerializeField]
+    private LayerMask groundedMask;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody>();
         camera = Camera.main.transform;    
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivityX);
+        HandleMovement();
+        if (Input.GetKeyDown("Jump") && isGrounded) {
+            isGrounded = false;
+            rigidbody.AddForce(transform.up * jumpForce);
+        }
+
+        // only will call with player in not grounded 
+        if (!isGrounded) {
+            CheckIfGrounded();
+        }
+        
     }
+    private void FixedUpdate() {
+        Movement();
+    }
+
+    // handles camera rotataion
+    private void HandleMovement() {
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.smoothDeltaTime * mouseSensitivityX);
+        verticalLookRotation += Input.GetAxis("Mouse Y") * Time.smoothDeltaTime * mouseSensitivityY;
+
+        // clamp vaules between locks -- add adjustable variable
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -20, 0);
+        camera.localEulerAngles = Vector3.left * verticalLookRotation;
+
+        // raw -- no native smoothing
+        // normalize clamps values
+        Vector3 moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        Vector3 targetMoveAmount = moveDirection * moveSpeed; // later add boost multiplier
+
+        // ref allows method to modify variable
+        // float smoothTime -- lower < Faster <> Slower > higher
+        // make adjustable
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, 0.15f);
+    }
+
+    private void Movement() {
+        // MovePosition -- world space
+        // player needs to move in local space
+        // modify moveAmout into local space with TransformDirection
+        rigidbody.MovePosition(rigidbody.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+    }
+
+    private void CheckIfGrounded() {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        // out allows the method to modify the variable &* in c++ 
+        // out does not need to be initialized
+        if (Physics.Raycast(ray, out hit, 1 + 0.1f, groundedMask)) {
+            isGrounded = true;
+        }
+    }
+
+
 }
---
+++ Assets/objects/planet/spawnPrefab.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPrefab : MonoBehaviour
+{
+    public int numObjects = 10;
+    public GameObject prefab;
+    private float planetRadius;
+   
+    void Start() {
+        planetRadius = GetComponent<SphereCollider>().radius;
+
+        SpawnPrefabs();
+    }
+
+    private void SpawnPrefabs() {
+        //var r = transform.localScale.x / 2;
+
+        for (int i = 0; i < numObjects; i++) {
+
+            Vector3 origin = transform.position;
+            Vector3 onPlanet = Random.onUnitSphere * planetRadius;
+
+            GameObject newGO = Instantiate(prefab, onPlanet, Quaternion.identity) as GameObject;
+            newGO.transform.LookAt(transform.position);
+            newGO.transform.rotation = newGO.transform.rotation * Quaternion.Euler(-90, 0, 0);
+
+        }
+
+    }
+}
+
+
