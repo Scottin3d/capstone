6bccc4867883e814fc34f82b9c99acea4346776e
Scottin3d <scottin3d@gmail.com>
2020-08-29 19:19:32 -0700

    UI

--- Assets/UIDriver.cs
+++ Assets/UIDriver.cs
@@ -5,37 +5,48 @@ using UnityEngine.UI;
 
 
 
-public class UIDriver : MonoBehaviour
-{
-
+public class UIDriver : MonoBehaviour {
 
     public Text speed;
-    public GameObject screenPoint;
-    private Vector3 playerPositionScreenPoint;
-    private Vector3 playerPositionWorldSpace;
-
-
     public playerAttributes playerAttributes;
-    public Transform player;
-    
+
+    // targeting
+    public RectTransform screenPoint;
+    public Transform targetObject;
+
+    public bool hit;
+    public RaycastHit raycastHit;
+
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         UpdateSpeed();
+    }
+
+    private void FixedUpdate() {
+       DrawLine(screenPoint.position, targetObject.position);
+        
     }
 
     public void UpdateSpeed() {
         speed.text = playerAttributes.speed.ToString();
-
-        //playerPositionScreenPoint = Camera.main.WorldToScreenPoint(screenPoint.transform.position);
-        playerPositionScreenPoint = screenPoint.transform.position;
-        playerPositionWorldSpace = Camera.main.WorldToScreenPoint(player.position);
-        DrawLine(playerPositionWorldSpace, playerPositionScreenPoint);
     }
 
     private void DrawLine(Vector3 toPoint, Vector3 fromPoint) {
+
         Debug.DrawLine(toPoint, fromPoint, Color.white);
+    }
+
+    private bool RaycastPoint() {
+        //RaycastHit hit;
+        Vector3 direction = targetObject.position - screenPoint.position;
+
+        if (Physics.Raycast(screenPoint.position, direction, out raycastHit)) {
+            if (raycastHit.transform.tag == "Planet") {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
